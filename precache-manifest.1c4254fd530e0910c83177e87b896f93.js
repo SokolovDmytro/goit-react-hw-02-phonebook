23abf566ecd038e2b8c15bc2f753e358cbb8db64
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-06-24 00:04:14 +0300

    Updates

---
+++ precache-manifest.1c4254fd530e0910c83177e87b896f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d73fc35fa19b52d91fcc6652d328a2",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "f962504179d7e545a5b1",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.66adf01f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.66adf01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ee61a8bcbad5f3c770",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.16abba3d.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
